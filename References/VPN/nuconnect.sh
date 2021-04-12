09f7668ef525b5e4c5d2eae3658f54bcfdb754c7
Kotaro Sonoda <sonoda@cis.nagasaki-u.ac.jp>
2021-04-12 16:52:55 +0900

    Update nuconnect.sh

--- References/VPN/nuconnect.sh
+++ References/VPN/nuconnect.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-export CISCO_SPLIT_INC = 1
-export CISCO_SPLIT_INC_0_ADDR = 133.45.0.0
-export CISCO_SPLIT_INC_0_MASK = 255.255.0.0
-export CISCO_SPLIT_INC_0_MASK_LEN = 16
-export VPNC_SCRIPT_DIR=/usr/share/vpnc-scripts
+export CISCO_SPLIT_INC=1
+export CISCO_SPLIT_INC_0_ADDR=133.45.0.0
+export CISCO_SPLIT_INC_0_MASK=255.255.0.0
+export CISCO_SPLIT_INC_0_MASK_LEN=16
+
+## Where is VPN Client Script?
+## if you use mac, comment off next line.
+#export VPNC_SCRIPT_DIR=/usr/local/etc
+## else if you use linux, comment off next line.
+export VPNC_SCRIPT_DIR=/usr/share/vpnc-scripts # Linux
 
 exec $VPNC_SCRIPT_DIR/vpnc-script
-
-
